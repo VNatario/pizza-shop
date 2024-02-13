df372bb88c3c87885df520c9675c794c3a4856ef
Vitor Natario <natariovitor@gmail.com>
2024-02-13 20:35:02 -0300

    lint: install ESLint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 export function App() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="flex h-screen items-center justify-center">
       <Button>Botão</Button>
     </div>
   )
