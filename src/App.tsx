c7f666024159c1d0e8e42975f89c9db9e2d128c5
Vitor Natario <natariovitor@gmail.com>
2024-02-26 13:20:54 -0300

    feat: use themeToggle for change themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors />
+        <Toaster richColors />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
